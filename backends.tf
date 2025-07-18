700ad2203040db481b138786b4e7f530678634bc
Vishal Vilasrao Ghule <91977540+anvi1505@users.noreply.github.com>
2025-07-18 11:15:22 +0530

    resource group updated in backends.tf

--- backends.tf
+++ backends.tf
@@ -2,7 +2,7 @@ terraform {
     backend "azurerm" {
         storage_account_name = "tfstatevish123"
         container_name = "prod-tf-state"
-        resource_group_name = "tf-lock"
+        resource_group_name = "strg"
         key = "project1.tfstate"
     }
 }
